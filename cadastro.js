4a788bc24cb77155f2506e8ab7c8505c32715d47
Jasco <jhjasco@hotmail.com>
2023-07-28 13:42:53 -0300

    continuando: adicição vars;

--- cadastro.js
+++ cadastro.js
@@ -1,4 +1,10 @@
+//Identificação
 let completeName;
 let nomeMae;
 let dataNascimento;
 let sexo;
+
+console.log(completeName);
+console.log(nomeMae);
+console.log(dataNascimento);
+console.log(sexo);
