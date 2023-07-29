28bc1640c366faef1a7e83fff8dc214b82c97ffd
Jasco <jhjasco@hotmail.com>
2023-07-28 23:49:24 -0300

    continuando: aprimorando formulário;

--- cadastro.js
+++ cadastro.js
@@ -1,10 +1,24 @@
 //Identificação
-let completeName;
-let nomeMae;
-let dataNascimento;
-let sexo;
+let completeName; //string
+let nomeMae; //string
+let dataNascimento;  //date(*)
+let sexo;  //symbol(*)
 
 console.log(completeName);
 console.log(nomeMae);
 console.log(dataNascimento);
 console.log(sexo);
+
+if (completeName != true) {
+    console.log("preencha o nome!");
+}
+
+//Endereço
+let logradouro; //string
+let numero; //integer/string
+let complemento; //string
+let bairro; //string
+
+//Contato
+let email; //string
+let telefone; //string/number
